36bc7c896aa58cbbb32c2691bd38f816decc8dae
Muhammed <skmdab@pm.me>
2023-07-24 04:55:48 +0530

    Update aws_create.sh

--- aws_create.sh
+++ aws_create.sh
@@ -41,11 +41,11 @@ INSTANCETYPE=t2.medium
 
 AMI_ID=ami-0f5ee92e2d63afc18
 
-ZONE=subnet-0f7ec0ece3a873bb1
+ZONE=subnet-058dfc6e7a1c552f2
 
 COUNTS=1
 
-INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count $COUNTS --instance-type $INSTANCETYPE --key-name filinta --security-group-ids sg-08a5b7d4856dedfe6 --subnet-id $ZONE --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCENAME'}]' --query 'Instances[0].InstanceId'  --output text)
+INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count $COUNTS --instance-type $INSTANCETYPE --key-name filinta --security-group-ids sg-04d7cf1bb1a5f765d --subnet-id $ZONE --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCENAME'}]' --query 'Instances[0].InstanceId'  --output text)
 
 progress_bar 75
 
